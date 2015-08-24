835096d081d78c48e12146ecfcb776f13a6ea51d
ricardo <ricardo@loadingplay.com>
2015-08-23 23:42:12 -0300

    added build

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt)
             options : { livereload : true },
             monkey: {
                 files: ['**/*.monkey', 'main.data/*.*'],
-                tasks: ['shell']
+                tasks: ['shell:target']
             }
         },
 
@@ -22,6 +22,9 @@ module.exports = function(grunt)
             },
             target: {
                 command: 'transcc_macos -target=Html5_Game main.monkey'
+            },
+            build: {
+                command: 'transcc_macos -target=Html5_Game -config=Release main.monkey'
             }
         },
 
@@ -34,12 +37,23 @@ module.exports = function(grunt)
                     livereload : true
                 }
             }
+        },
+
+
+        compress: {
+            main: 
+            {
+                options: { archive: 'builds/' + grunt.template.today('yyyy-mm-dd') + '.zip' },
+                files: [{expand: true, cwd: 'main.buildv82a/html5', src: ['**'], dest: ''}]
+            }
         }
 
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-express');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
-    grunt.registerTask('default', ['shell', 'express', 'watch']);
+    grunt.registerTask('build', ['shell:build', 'compress']);
+    grunt.registerTask('default', ['shell:target', 'express', 'watch']);
 };
